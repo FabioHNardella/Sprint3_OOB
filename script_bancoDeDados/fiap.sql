064e1e3316dac33b053a3a473b2c4b5d0f208261
FabioHNardella <102842907+FabioHNardella@users.noreply.github.com>
2025-08-25 11:44:10 -0300

    Add files via upload

---
+++ script_bancoDeDados/fiap.sql
@@ -0,0 +1,25 @@
+CREATE TABLE novo_colaborador(
+    id_novo_colaborador NUMBER(5) PRIMARY KEY,
+    nome_colaborador VARCHAR2(25),
+    sobrenome_colaborador VARCHAR2(25),
+    email VARCHAR2(25),
+    telefone VARCHAR2(25)
+);
+
+CREATE TABLE buddy(
+    id_novo_colaborador NUMBER(5) PRIMARY KEY,
+    nome_colaborador VARCHAR2(25),
+    sobrenome_colaborador VARCHAR2(25),
+    email VARCHAR2(25),
+    telefone VARCHAR2(25)
+);
+
+CREATE TABLE gestor(
+    id_novo_colaborador NUMBER(5) PRIMARY KEY,
+    nome_colaborador VARCHAR2(25),
+    sobrenome_colaborador VARCHAR2(25),
+    email VARCHAR2(25),
+    telefone VARCHAR2(25)
+);
+
+
