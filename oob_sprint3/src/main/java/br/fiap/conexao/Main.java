51b47a5adbc5d89e3313328c7d41bc9e96e3bd99
FabioHNardella <102842907+FabioHNardella@users.noreply.github.com>
2025-08-30 01:44:15 -0300

    Add files via upload

--- oob_sprint3/src/main/java/br/fiap/conexao/Main.java
+++ oob_sprint3/src/main/java/br/fiap/conexao/Main.java
@@ -1,9 +1,23 @@
 package br.fiap.conexao;
 
+import br.fiap.modelo.Gestor;
+import br.fiap.modelo.GestorDAO;
+
 public class Main {
-    int opcao;
+    public static void main(String[] args) {
+        GestorDAO gestDAO = new GestorDAO();
 
+        gestDAO.inserir(new Gestor(1L, "Gustavo", "Burro", "mariana.almeida@empresa.com", "(11) 98877-1234"));
+        gestDAO.inserir(new Gestor(2L, "Caio", "Patto", "caio.patto@empresa.com", "(21) 97766-4321"));
+        gestDAO.inserir(new Gestor(3L, "Rafah", "Sargento", "rafah.sargento@empresa.com", "(31) 99655-6789"));
+        gestDAO.inserir(new Gestor(4L, "Gustavo", "Pereira", "gustavo.pereira@empresa.com", "(41) 98444-1122"));
+        gestDAO.inserir(new Gestor(5L, "Gabriel", "OpenToWork", "open.towork@empresa.com", "(51) 97233-5566"));
 
+        gestDAO.atualizar(1L, new Gestor(1L, "Fabio", "Mortadella", "fabio.nardella@empresa.com", "(11) 92233-4455"));
 
+        gestDAO.listar();
 
+        gestDAO.deletar(1L);
+
+    }
 }
